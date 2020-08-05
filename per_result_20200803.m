a48c3a6e9d43623adccd07acb5086910b0a8514f
room317 <room317@gmail.com>
2020-08-05 10:16:25 +0900

    - Simulation results for wider subcarrier spacing, eg. 30 kHz and 60 kHz, added.

---
+++ per_result_20200803.m
@@ -0,0 +1,508 @@
+% simulator: new_wave_test_r3
+% commit: caa3e60
+
+% RUNNING CASE: OFDM BW10 SCS15 FC4000 V3 DS0.1 TDLC MCS8 LEN6120 SIM10000 SNR0 TFNR TFEQMMSE
+per_case01 = [ ...
+     0.0        401   1.000000
+     2.0        401   1.000000
+     4.0        682   0.587977
+     6.0       1816   0.220815
+     8.0       5669   0.070736
+    10.0      10000   0.017000
+    12.0      10000   0.003900
+    14.0      10000   0.000100];
+
+% RUNNING CASE: OFDM BW10 SCS15 FC4000 V120 DS0.1 TDLC MCS8 LEN6120 SIM10000 SNR0 TFNR TFEQMMSE
+per_case02 = [ ...
+     0.0        401   1.000000
+     2.0        401   1.000000
+     4.0        585   0.685470
+     6.0       1910   0.209948
+     8.0      10000   0.034700
+    10.0      10000   0.003200
+    12.0      10000   0.000200];
+
+% RUNNING CASE: OFDM BW10 SCS15 FC4000 V200 DS0.1 TDLC MCS8 LEN6120 SIM10000 SNR0 TFNR TFEQMMSE
+per_case03 = [ ...
+     0.0        401   1.000000
+     2.0        401   1.000000
+     4.0        493   0.813387
+     6.0       1864   0.215129
+     8.0      10000   0.029400
+    10.0      10000   0.001500
+    12.0      10000   0.000000];
+
+% RUNNING CASE: OFDM BW10 SCS15 FC4000 V500 DS0.1 TDLC MCS8 LEN6120 SIM10000 SNR0 TFNR TFEQMMSE
+per_case04 = [ ...
+     0.0        401   1.000000
+     2.0        401   1.000000
+     4.0        402   0.997512
+     6.0        466   0.860515
+     8.0        739   0.542625
+    10.0       1116   0.359319
+    12.0       1530   0.262092
+    14.0       1493   0.268587
+    16.0       1649   0.243178
+    18.0       1399   0.286633];
+
+% RUNNING CASE: OFDM BW10 SCS15 FC4000 V3 DS0.1 TDLE MCS8 LEN6120 SIM10000 SNR0 TFNR TFEQMMSE
+per_case05 = [ ...
+     0.0        401   1.000000
+     2.0        401   1.000000
+     4.0        552   0.726449
+     6.0       2243   0.178778
+     8.0      10000   0.028200
+    10.0      10000   0.002700
+    12.0      10000   0.000300];
+
+% RUNNING CASE: OFDM BW10 SCS15 FC4000 V120 DS0.1 TDLE MCS8 LEN6120 SIM10000 SNR0 TFNR TFEQMMSE
+per_case06 = [ ...
+     0.0        401   1.000000
+     2.0        401   1.000000
+     4.0        501   0.800399
+     6.0       2305   0.173970
+     8.0      10000   0.014600
+    10.0      10000   0.000400];
+
+% RUNNING CASE: OFDM BW10 SCS15 FC4000 V200 DS0.1 TDLE MCS8 LEN6120 SIM10000 SNR0 TFNR TFEQMMSE
+per_case07 = [ ...
+     0.0        401   1.000000
+     2.0        401   1.000000
+     4.0        454   0.883260
+     6.0       2115   0.189598
+     8.0      10000   0.008900
+    10.0      10000   0.000100];
+
+% RUNNING CASE: OFDM BW10 SCS15 FC4000 V500 DS0.1 TDLE MCS8 LEN6120 SIM10000 SNR0 TFNR TFEQMMSE
+per_case08 = [ ...
+     0.0        401   1.000000
+     2.0        401   1.000000
+     4.0        401   1.000000
+     6.0        401   1.000000
+     8.0        415   0.966265
+    10.0        769   0.521456
+    12.0       3444   0.116434
+    14.0      10000   0.032900
+    16.0      10000   0.024100
+    18.0      10000   0.031900];
+
+% RUNNING CASE: OFDM BW20 SCS30 FC4000 V3 DS0.1 TDLC MCS8 LEN6120 SIM10000 SNR0 TFNR TFEQMMSE
+per_case09 = [ ...
+     0.0        401   1.000000
+     2.0        401   1.000000
+     4.0        552   0.726449
+     6.0       2184   0.183608
+     8.0      10000   0.031900
+    10.0      10000   0.004600
+    12.0      10000   0.000400];
+
+% RUNNING CASE: OFDM BW20 SCS30 FC4000 V120 DS0.1 TDLC MCS8 LEN6120 SIM10000 SNR0 TFNR TFEQMMSE
+per_case10 = [ ...
+     0.0        401   1.000000
+     2.0        401   1.000000
+     4.0        525   0.763810
+     6.0       2290   0.175109
+     8.0      10000   0.024800
+    10.0      10000   0.001500
+    12.0      10000   0.000100];
+
+% RUNNING CASE: OFDM BW20 SCS30 FC4000 V200 DS0.1 TDLC MCS8 LEN6120 SIM10000 SNR0 TFNR TFEQMMSE
+per_case11 = [ ...
+     0.0        401   1.000000
+     2.0        401   1.000000
+     4.0        507   0.790927
+     6.0       2300   0.174348
+     8.0      10000   0.017300
+    10.0      10000   0.001300
+    12.0      10000   0.000100];
+
+% RUNNING CASE: OFDM BW20 SCS30 FC4000 V500 DS0.1 TDLC MCS8 LEN6120 SIM10000 SNR0 TFNR TFEQMMSE
+per_case12 = [ ...
+     0.0        401   1.000000
+     2.0        401   1.000000
+     4.0        438   0.915525
+     6.0       1898   0.211275
+     8.0      10000   0.012700
+    10.0      10000   0.000300];
+
+% RUNNING CASE: OFDM BW20 SCS30 FC4000 V3 DS0.1 TDLE MCS8 LEN6120 SIM10000 SNR0 TFNR TFEQMMSE
+per_case13 = [ ...
+     0.0        401   1.000000
+     2.0        401   1.000000
+     4.0        505   0.794059
+     6.0       2531   0.158435
+     8.0      10000   0.016500
+    10.0      10000   0.000300];
+
+% RUNNING CASE: OFDM BW20 SCS30 FC4000 V120 DS0.1 TDLE MCS8 LEN6120 SIM10000 SNR0 TFNR TFEQMMSE
+per_case14 = [ ...
+     0.0        401   1.000000
+     2.0        401   1.000000
+     4.0        478   0.838912
+     6.0       2872   0.139624
+     8.0      10000   0.009000
+    10.0      10000   0.000100];
+
+% RUNNING CASE: OFDM BW20 SCS30 FC4000 V200 DS0.1 TDLE MCS8 LEN6120 SIM10000 SNR0 TFNR TFEQMMSE
+per_case15 = [ ...
+     0.0        401   1.000000
+     2.0        401   1.000000
+     4.0        473   0.847780
+     6.0       3078   0.130279
+     8.0      10000   0.006900
+    10.0      10000   0.000000];
+
+% RUNNING CASE: OFDM BW20 SCS30 FC4000 V500 DS0.1 TDLE MCS8 LEN6120 SIM10000 SNR0 TFNR TFEQMMSE
+per_case16 = [ ...
+     0.0        401   1.000000
+     2.0        401   1.000000
+     4.0        416   0.963942
+     6.0       1944   0.206276
+     8.0      10000   0.005300
+    10.0      10000   0.000100];
+
+% RUNNING CASE: OFDM BW40 SCS60 FC4000 V3 DS0.1 TDLC MCS8 LEN6120 SIM10000 SNR0 TFNR TFEQMMSE
+per_case17 = [ ...
+     0.0        401   1.000000
+     2.0        401   1.000000
+     4.0        501   0.800399
+     6.0       2472   0.162217
+     8.0      10000   0.016800
+    10.0      10000   0.001400
+    12.0      10000   0.000000];
+
+% RUNNING CASE: OFDM BW40 SCS60 FC4000 V120 DS0.1 TDLC MCS8 LEN6120 SIM10000 SNR0 TFNR TFEQMMSE
+per_case18 = [ ...
+     0.0        401   1.000000
+     2.0        401   1.000000
+     4.0        509   0.787819
+     6.0       2348   0.170784
+     8.0      10000   0.014200
+    10.0      10000   0.000300];
+
+% RUNNING CASE: OFDM BW40 SCS60 FC4000 V200 DS0.1 TDLC MCS8 LEN6120 SIM10000 SNR0 TFNR TFEQMMSE
+per_case19 = [ ...
+     0.0        401   1.000000
+     2.0        401   1.000000
+     4.0        482   0.831950
+     6.0       2682   0.149515
+     8.0      10000   0.009600
+    10.0      10000   0.000300];
+
+% RUNNING CASE: OFDM BW40 SCS60 FC4000 V500 DS0.1 TDLC MCS8 LEN6120 SIM10000 SNR0 TFNR TFEQMMSE
+per_case20 = [ ...
+     0.0        401   1.000000
+     2.0        401   1.000000
+     4.0        457   0.877462
+     6.0       2700   0.148519
+     8.0      10000   0.003800
+    10.0      10000   0.000200];
+
+% RUNNING CASE: OFDM BW40 SCS60 FC4000 V3 DS0.1 TDLE MCS8 LEN6120 SIM10000 SNR0 TFNR TFEQMMSE
+per_case21 = [ ...
+     0.0        401   1.000000
+     2.0        401   1.000000
+     4.0        484   0.828512
+     6.0       2643   0.151722
+     8.0      10000   0.011300
+    10.0      10000   0.000000];
+
+% RUNNING CASE: OFDM BW40 SCS60 FC4000 V120 DS0.1 TDLE MCS8 LEN6120 SIM10000 SNR0 TFNR TFEQMMSE
+per_case22 = [ ...
+     0.0        401   1.000000
+     2.0        401   1.000000
+     4.0        467   0.858672
+     6.0       3008   0.133311
+     8.0      10000   0.008600
+    10.0      10000   0.000100];
+
+% RUNNING CASE: OFDM BW40 SCS60 FC4000 V200 DS0.1 TDLE MCS8 LEN6120 SIM10000 SNR0 TFNR TFEQMMSE
+per_case23 = [ ...
+     0.0        401   1.000000
+     2.0        401   1.000000
+     4.0        476   0.842437
+     6.0       2832   0.141596
+     8.0      10000   0.007300
+    10.0      10000   0.000100];
+
+% RUNNING CASE: OFDM BW40 SCS60 FC4000 V500 DS0.1 TDLE MCS8 LEN6120 SIM10000 SNR0 TFNR TFEQMMSE
+per_case24 = [ ...
+     0.0        401   1.000000
+     2.0        401   1.000000
+     4.0        450   0.891111
+     6.0       2908   0.137895
+     8.0      10000   0.002600
+    10.0      10000   0.000000];
+
+% % RUNNING CASE: OFDM BW10 FC4000 V3 DS0.1 TDLA MCS8 LEN6120 SIM10000 SNR0 REAL TFEQMMSE
+% per_case25 = [ ...
+%   ];
+% 
+% % RUNNING CASE: OFDM BW10 FC4000 V120 DS0.1 TDLA MCS8 LEN6120 SIM10000 SNR0 REAL TFEQMMSE
+% per_case26 = [ ...
+%   ];
+% 
+% % RUNNING CASE: OFDM BW10 FC4000 V200 DS0.1 TDLA MCS8 LEN6120 SIM10000 SNR0 REAL TFEQMMSE
+% per_case27 = [ ...
+%   ];
+% 
+% % RUNNING CASE: OFDM BW10 FC4000 V500 DS0.1 TDLA MCS8 LEN6120 SIM10000 SNR0 REAL TFEQMMSE
+% per_case28 = [ ...
+%   ];
+% 
+% % RUNNING CASE: OFDM BW10 FC4000 V3 DS0.1 TDLD MCS8 LEN6120 SIM10000 SNR0 REAL TFEQMMSE
+% per_case29 = [ ...
+%   ];
+% 
+% % RUNNING CASE: OFDM BW10 FC4000 V120 DS0.1 TDLD MCS8 LEN6120 SIM10000 SNR0 REAL TFEQMMSE
+% per_case30 = [ ...
+%   ];
+% 
+% % RUNNING CASE: OFDM BW10 FC4000 V200 DS0.1 TDLD MCS8 LEN6120 SIM10000 SNR0 REAL TFEQMMSE
+% per_case31 = [ ...
+%   ];
+% 
+% % RUNNING CASE: OFDM BW10 FC4000 V500 DS0.1 TDLD MCS8 LEN6120 SIM10000 SNR0 REAL TFEQMMSE
+% per_case32 = [ ...
+%   ];
+% 
+% % RUNNING CASE: OTFS BW10 FC4000 V3 DS0.1 TDLA MCS8 LEN6120 SIM10000 SNR0 DDTONE TFEQMMSE
+% per_case33 = [ ...
+%   ];
+% 
+% % RUNNING CASE: OTFS BW10 FC4000 V120 DS0.1 TDLA MCS8 LEN6120 SIM10000 SNR0 DDTONE TFEQMMSE
+% per_case34 = [ ...
+%   ];
+% 
+% % RUNNING CASE: OTFS BW10 FC4000 V200 DS0.1 TDLA MCS8 LEN6120 SIM10000 SNR0 DDTONE TFEQMMSE
+% per_case35 = [ ...
+%   ];
+% 
+% % RUNNING CASE: OTFS BW10 FC4000 V500 DS0.1 TDLA MCS8 LEN6120 SIM10000 SNR0 DDTONE TFEQMMSE
+% per_case36 = [ ...
+%   ];
+% 
+% % RUNNING CASE: OTFS BW10 FC4000 V3 DS0.1 TDLD MCS8 LEN6120 SIM10000 SNR0 DDTONE TFEQMMSE
+% per_case37 = [ ...
+%   ];
+% 
+% % RUNNING CASE: OTFS BW10 FC4000 V120 DS0.1 TDLD MCS8 LEN6120 SIM10000 SNR0 DDTONE TFEQMMSE
+% per_case38 = [ ...
+%   ];
+% 
+% % RUNNING CASE: OTFS BW10 FC4000 V200 DS0.1 TDLD MCS8 LEN6120 SIM10000 SNR0 DDTONE TFEQMMSE
+% per_case39 = [ ...
+%   ];
+% 
+% % RUNNING CASE: OTFS BW10 FC4000 V500 DS0.1 TDLD MCS8 LEN6120 SIM10000 SNR0 DDTONE TFEQMMSE
+% per_case40 = [ ...
+%   ];
+% 
+% % RUNNING CASE: OTFS BW10 FC4000 V3 DS0.1 TDLA MCS8 LEN6120 SIM10000 SNR0 REAL TFEQMMSE
+% per_case41 = [ ...
+%   ];
+% 
+% % RUNNING CASE: OTFS BW10 FC4000 V120 DS0.1 TDLA MCS8 LEN6120 SIM10000 SNR0 REAL TFEQMMSE
+% per_case42 = [ ...
+%   ];
+%  
+% % RUNNING CASE: OTFS BW10 FC4000 V200 DS0.1 TDLA MCS8 LEN6120 SIM10000 SNR0 REAL TFEQMMSE
+% per_case43 = [ ...
+%   ];
+%  
+% % RUNNING CASE: OTFS BW10 FC4000 V500 DS0.1 TDLA MCS8 LEN6120 SIM10000 SNR0 REAL TFEQMMSE
+% per_case44 = [ ...
+%   ];
+% 
+% % RUNNING CASE: OTFS BW10 FC4000 V3 DS0.1 TDLD MCS8 LEN6120 SIM10000 SNR0 REAL TFEQMMSE
+% per_case45 = [ ...
+%   ];
+% 
+% % RUNNING CASE: OTFS BW10 FC4000 V120 DS0.1 TDLD MCS8 LEN6120 SIM10000 SNR0 REAL TFEQMMSE
+% per_case46 = [ ...
+%   ];
+% 
+% % RUNNING CASE: OTFS BW10 FC4000 V200 DS0.1 TDLD MCS8 LEN6120 SIM10000 SNR0 REAL TFEQMMSE
+% per_case47 = [ ...
+%   ];
+% 
+% % RUNNING CASE: OTFS BW10 FC4000 V500 DS0.1 TDLD MCS8 LEN6120 SIM10000 SNR0 REAL TFEQMMSE
+% per_case48 = [ ...
+%   ];
+% 
+% % RUNNING CASE: OTFS BW10 FC4000 V3 DS0.1 TDLA MCS8 LEN6120 SIM10000 SNR0 DDTONE TFEQMMSE
+% per_case49 = [ ...
+%   ];
+% 
+% % RUNNING CASE: OTFS BW10 FC4000 V120 DS0.1 TDLA MCS8 LEN6120 SIM10000 SNR0 DDTONE TFEQMMSE
+% per_case50 = [ ...
+%   ];
+% 
+% % RUNNING CASE: OTFS BW10 FC4000 V200 DS0.1 TDLA MCS8 LEN6120 SIM10000 SNR0 DDTONE TFEQMMSE
+% per_case51 = [ ...
+%   ];
+% 
+% % RUNNING CASE: OTFS BW10 FC4000 V500 DS0.1 TDLA MCS8 LEN6120 SIM10000 SNR0 DDTONE TFEQMMSE
+% per_case52 = [ ...
+%   ];
+% 
+% % RUNNING CASE: OTFS BW10 FC4000 V3 DS0.1 TDLA MCS8 LEN6120 SIM10000 SNR0 REAL TFEQMMSE
+% per_case53 = [ ...
+%   ];
+%  
+% % RUNNING CASE: OTFS BW10 FC4000 V120 DS0.1 TDLA MCS8 LEN6120 SIM10000 SNR0 REAL TFEQMMSE
+% per_case54 = [ ...
+%   ];
+%  
+% % RUNNING CASE: OTFS BW10 FC4000 V200 DS0.1 TDLA MCS8 LEN6120 SIM10000 SNR0 REAL TFEQMMSE
+% per_case55 = [ ...
+%   ];
+%  
+% % RUNNING CASE: OTFS BW10 FC4000 V500 DS0.1 TDLA MCS8 LEN6120 SIM10000 SNR0 REAL TFEQMMSE
+% per_case56 = [ ...
+%   ];
+% 
+% % RUNNING CASE: OTFS BW10 FC4000 V3 DS0.1 TDLA MCS8 LEN6120 SIM10000 SNR0 REAL TFEQZF
+% per_case57 = [ ...
+%   ];
+
+figure
+semilogy(per_case01(:,1),per_case01(:,3),'-ko'), hold on
+semilogy(per_case02(:,1),per_case02(:,3),'-kx')
+semilogy(per_case03(:,1),per_case03(:,3),'-ks')
+semilogy(per_case04(:,1),per_case04(:,3),'-k^'), hold off
+grid minor
+title('OFDM BW10 SCS15 FC4000 DS0.1 TDLC MCS8 LEN6120 TFNR TFEQMMSE')
+xlabel('SNR (dB)'), ylabel('PER')
+legend('V3', 'V120', 'V200', 'V500')
+axis([-1 19 1e-3 1])
+
+figure
+semilogy(per_case05(:,1),per_case05(:,3),'-ko'), hold on
+semilogy(per_case06(:,1),per_case06(:,3),'-kx')
+semilogy(per_case07(:,1),per_case07(:,3),'-ks')
+semilogy(per_case08(:,1),per_case08(:,3),'-k^'), hold off
+grid minor
+title('OFDM BW10 SCS15 FC4000 DS0.1 TDLE MCS8 LEN6120 TFNR TFEQMMSE')
+xlabel('SNR (dB)'), ylabel('PER')
+legend('V3', 'V120', 'V200', 'V500')
+axis([-1 19 1e-3 1])
+
+figure
+semilogy(per_case09(:,1),per_case09(:,3),'-ko'), hold on
+semilogy(per_case10(:,1),per_case10(:,3),'-kx')
+semilogy(per_case11(:,1),per_case11(:,3),'-ks')
+semilogy(per_case12(:,1),per_case12(:,3),'-k^'), hold off
+grid minor
+title('OFDM BW20 SCS30 FC4000 DS0.1 TDLC MCS8 LEN6120 TFNR TFEQMMSE')
+xlabel('SNR (dB)'), ylabel('PER')
+legend('V3', 'V120', 'V200', 'V500')
+axis([-1 19 1e-3 1])
+
+figure
+semilogy(per_case13(:,1),per_case13(:,3),'-ko'), hold on
+semilogy(per_case14(:,1),per_case14(:,3),'-kx')
+semilogy(per_case15(:,1),per_case15(:,3),'-ks')
+semilogy(per_case16(:,1),per_case16(:,3),'-k^'), hold off
+grid minor
+title('OFDM BW20 SCS30 FC4000 DS0.1 TDLE MCS8 LEN6120 TFNR TFEQMMSE')
+xlabel('SNR (dB)'), ylabel('PER')
+legend('V3', 'V120', 'V200', 'V500')
+axis([-1 19 1e-3 1])
+
+figure
+semilogy(per_case17(:,1),per_case17(:,3),'-ko'), hold on
+semilogy(per_case18(:,1),per_case18(:,3),'-kx')
+semilogy(per_case19(:,1),per_case19(:,3),'-ks')
+semilogy(per_case20(:,1),per_case20(:,3),'-k^'), hold off
+grid minor
+title('OFDM BW40 SCS60 FC4000 DS0.1 TDLC MCS8 LEN6120 TFNR TFEQMMSE')
+xlabel('SNR (dB)'), ylabel('PER')
+legend('V3', 'V120', 'V200', 'V500')
+axis([-1 19 1e-3 1])
+
+figure
+semilogy(per_case21(:,1),per_case21(:,3),'-ko'), hold on
+semilogy(per_case22(:,1),per_case22(:,3),'-kx')
+semilogy(per_case23(:,1),per_case23(:,3),'-ks')
+semilogy(per_case24(:,1),per_case24(:,3),'-k^'), hold off
+grid minor
+title('OFDM BW40 SCS60 FC4000 DS0.1 TDLE MCS8 LEN6120 TFNR TFEQMMSE')
+xlabel('SNR (dB)'), ylabel('PER')
+legend('V3', 'V120', 'V200', 'V500')
+axis([-1 19 1e-3 1])
+
+% figure
+% semilogy(per_case25(:,1),per_case25(:,3),'-ko'), hold on
+% semilogy(per_case26(:,1),per_case26(:,3),'-kx')
+% semilogy(per_case27(:,1),per_case27(:,3),'-ks')
+% semilogy(per_case28(:,1),per_case28(:,3),'-k^'), hold off
+% grid minor
+% title('OFDM BW10 FC4000 DS0.1 TDLA MCS8 LEN6120 REAL TFEQMMSE')
+% xlabel('SNR (dB)'), ylabel('PER')
+% legend('V3', 'V120', 'V200', 'V500')
+% axis([-1 19 1e-3 1])
+% 
+% figure
+% semilogy(per_case29(:,1),per_case29(:,3),'-ko'), hold on
+% semilogy(per_case30(:,1),per_case30(:,3),'-kx')
+% semilogy(per_case31(:,1),per_case31(:,3),'-ks')
+% semilogy(per_case32(:,1),per_case32(:,3),'-k^'), hold off
+% grid minor
+% title('OFDM BW10 FC4000 DS0.1 TDLD MCS8 LEN6120 REAL TFEQMMSE')
+% xlabel('SNR (dB)'), ylabel('PER')
+% legend('V3', 'V120', 'V200', 'V500')
+% axis([-1 19 1e-3 1])
+% 
+% figure
+% semilogy(per_case33(:,1),per_case33(:,3),'-ko'), hold on
+% semilogy(per_case34(:,1),per_case34(:,3),'-kx')
+% semilogy(per_case35(:,1),per_case35(:,3),'-ks')
+% semilogy(per_case36(:,1),per_case36(:,3),'-k^'), hold off
+% grid minor
+% title('OTFS BW10 FC4000 DS0.1 TDLA MCS8 LEN6120 DDTONE TFEQMMSE')
+% xlabel('SNR (dB)'), ylabel('PER')
+% legend('V3', 'V120', 'V200', 'V500')
+% axis([-1 19 1e-3 1])
+% 
+% figure
+% semilogy(per_case37(:,1),per_case37(:,3),'-ko'), hold on
+% semilogy(per_case38(:,1),per_case38(:,3),'-kx')
+% semilogy(per_case39(:,1),per_case39(:,3),'-ks')
+% semilogy(per_case40(:,1),per_case40(:,3),'-k^'), hold off
+% grid minor
+% title('OTFS BW10 FC4000 DS0.1 TDLD MCS8 LEN6120 DDTONE TFEQMMSE')
+% xlabel('SNR (dB)'), ylabel('PER')
+% legend('V3', 'V120', 'V200', 'V500')
+% axis([-1 19 1e-3 1])
+% 
+% figure
+% semilogy(per_case41(:,1),per_case41(:,3),'-ko'), hold on
+% semilogy(per_case42(:,1),per_case42(:,3),'-kx')
+% semilogy(per_case43(:,1),per_case43(:,3),'-ks')
+% semilogy(per_case44(:,1),per_case44(:,3),'-k^'), hold off
+% grid minor
+% title('OTFS BW10 FC4000 DS0.1 TDLA MCS8 LEN6120 REAL TFEQMMSE')
+% xlabel('SNR (dB)'), ylabel('PER')
+% legend('V3', 'V120', 'V200', 'V500')
+% axis([-1 19 1e-3 1])
+% 
+% figure
+% semilogy(per_case45(:,1),per_case45(:,3),'-ko'), hold on
+% semilogy(per_case46(:,1),per_case46(:,3),'-kx')
+% semilogy(per_case47(:,1),per_case47(:,3),'-ks')
+% semilogy(per_case48(:,1),per_case48(:,3),'-k^'), hold off
+% grid minor
+% title('OTFS BW10 FC4000 DS0.1 TDLD MCS8 LEN6120 REAL TFEQMMSE')
+% xlabel('SNR (dB)'), ylabel('PER')
+% legend('V3', 'V120', 'V200', 'V500')
+% axis([-1 19 1e-3 1])
+% 
+% figure
+% semilogy(per_case53(:,1),per_case53(:,3),'-ko'), hold on
+% semilogy(per_case57(:,1),per_case57(:,3),'-kx'), hold off
+% grid minor
+% title('OTFS BW10 FC4000 V3 DS0.1 TDLA MCS8 LEN6120 REAL')
+% xlabel('SNR (dB)'), ylabel('PER')
+% legend('MMSE EQ', 'ZF EQ')
+% axis([-1 19 1e-3 1])
